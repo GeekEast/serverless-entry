2cce94b184e08649ff5fede53b14099caf4601b3
GeekEast <sx.ba2017@gmail.com>
2019-10-22 23:37:00 +1030

    add router for get with parameters

--- handler.ts
+++ handler.ts
@@ -2,10 +2,11 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
+  const name = event.pathParameters.name;
   return {
     statusCode: 200,
     body: JSON.stringify({
-      event: "Hello World",
+      name: name,
     }),
   };
 }
